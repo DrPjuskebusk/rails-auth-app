88821cb70119eaa0f8450305ed9a50d902cc696b
Nicolai Borup Christensen <nc.nexo@gmail.com>
2025-03-21 09:46:35 +0100

    Views, admin, roles, policy
    Created views for authentication, password change, mails etc.. created an admin and user role, added to user model and migrated to database, created a dashboard policy for users and admins as well as changed minimum password length.

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,10 @@
+class AdminController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authorize_admin
+  def index
+  end
+  private
+  def authorize_admin # Authorizes if the user is an admin and gives an alert if not.
+    redirect_to root_path, alert: "Access Denied Bitch!!!" unless current_user&.admin?
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "admin/index"
   root "dashboard#index"
   devise_for :users
   get "admin", to: "admin#index", as: :admin_dashboard
